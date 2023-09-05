f26a125234a5ded266e630d21b66207fd924244c
Ivan Maia <ivanleonardo_maia@hotmail.com>
2023-09-05 14:35:55 -0300

    Generecs Set Map - HashCode e Equals personalizados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,20 @@
 package application;
 
+import entities.Client;
 
 public class Program {
 	public static void main(String[] args) {
 				
-		/*HashCode
-		Método que retorna um número inteiro representando um código gerado
-		a partir das informações do objeto*/
+		/*HashCode e Equals personalizados*/
 		
-		String a = "Maria";
-		String b = "Alex";
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Maria", "maria@gmail.com");
 		
-		System.out.println(a.hashCode());
-		System.out.println(b.hashCode());
-	
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		
+		System.out.println(c1 == c2); 
 		
 	}
 }
