1e57fda4157de2e6e544de1042ea41d1534b863e
Ivan Maia <ivanleonardo_maia@hotmail.com>
2023-09-05 14:16:56 -0300

    Generecs Set Map - hashCode e equals (HashCode)

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,11 +3,17 @@ package application;
 
 public class Program {
 	public static void main(String[] args) {
+				
+		/*HashCode
+		Método que retorna um número inteiro representando um código gerado
+		a partir das informações do objeto*/
 		
 		String a = "Maria";
 		String b = "Alex";
 		
-		System.out.println(a.equals(b));
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+	
 		
 	}
 }
